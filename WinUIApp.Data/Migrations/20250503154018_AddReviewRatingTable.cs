0798825d40593a9a06c5fb0ac38cdeb342e0673c
Leonard <fdavidliviu@yahoo.ro>
2025-05-03 18:42:56 +0300

    migration

---
+++ WinUIApp.Data/Migrations/20250503154018_AddReviewRatingTable.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WinUIApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReviewRatingTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ratings",
+                columns: table => new
+                {
+                    RatingID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DrinkID = table.Column<int>(type: "int", nullable: false),
+                    UserID = table.Column<int>(type: "int", nullable: false),
+                    RatingValue = table.Column<double>(type: "float", nullable: true),
+                    RatingDate = table.Column<DateTime>(type: "datetime", nullable: true),
+                    IsActive = table.Column<byte>(type: "tinyint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ratings", x => x.RatingID);
+                    table.ForeignKey(
+                        name: "FK_Ratings_Drinks_DrinkID",
+                        column: x => x.DrinkID,
+                        principalTable: "Drinks",
+                        principalColumn: "DrinkId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Ratings_Users_UserID",
+                        column: x => x.UserID,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reviews",
+                columns: table => new
+                {
+                    ReviewID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RatingID = table.Column<int>(type: "int", nullable: false),
+                    UserID = table.Column<int>(type: "int", nullable: true),
+                    Content = table.Column<string>(type: "text", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "datetime", nullable: true),
+                    IsActive = table.Column<byte>(type: "tinyint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reviews", x => x.ReviewID);
+                    table.ForeignKey(
+                        name: "FK_Reviews_Ratings_RatingID",
+                        column: x => x.RatingID,
+                        principalTable: "Ratings",
+                        principalColumn: "RatingID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Reviews_Users_UserID",
+                        column: x => x.UserID,
+                        principalTable: "Users",
+                        principalColumn: "UserId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ratings_DrinkID",
+                table: "Ratings",
+                column: "DrinkID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ratings_UserID",
+                table: "Ratings",
+                column: "UserID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_RatingID",
+                table: "Reviews",
+                column: "RatingID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_UserID",
+                table: "Reviews",
+                column: "UserID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reviews");
+
+            migrationBuilder.DropTable(
+                name: "Ratings");
+        }
+    }
+}
