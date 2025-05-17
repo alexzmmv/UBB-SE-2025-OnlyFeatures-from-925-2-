703ebb97807490a9ae5f4cf42fa0b972278d6024
Leonard <fdavidliviu@yahoo.ro>
2025-05-18 00:36:41 +0300

    bare bones detail page; has no user and admin checks

---
+++ WinUIApp.WebUI/Controllers/DrinkController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using WinUIApp.ProxyServices;
+using WinUIApp.ProxyServices.Models;
+
+namespace WinUIApp.WebUI.Controllers
+{
+    public class DrinkController : Controller
+    {
+        private readonly IDrinkService drinkService;
+        private readonly IReviewService reviewService;
+        private readonly IRatingService ratingService;
+
+        public DrinkController(IDrinkService drinkService, IReviewService reviewService, IRatingService ratingService)
+        {
+            this.drinkService = drinkService;
+            this.reviewService = reviewService;
+            this.ratingService = ratingService;
+        }
+
+        public IActionResult DrinkDetail(int id)
+        {
+            var drink = drinkService.GetDrinkById(id);
+            var ratings = ratingService.GetRatingsByProduct(id);
+            var reviews = new List<Review>();
+            foreach (var rating in ratings)
+            {
+                var ratingReviews = reviewService.GetReviewsByRating(rating.RatingId); 
+                reviews.AddRange(ratingReviews);
+            }
+
+            var viewModel = new DrinkDetailViewModel
+            {
+                Drink = drink,
+                CategoriesDisplay = string.Join(", ", drink.CategoryList.Select(c => c.CategoryName)),
+                AverageRatingScore = ratingService.GetAverageRating(id),
+                Reviews = reviews,
+            };
+
+            return View(viewModel);
+        }
+    }
+} 
