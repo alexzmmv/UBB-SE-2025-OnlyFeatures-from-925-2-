3ba6d8946a563cddb9c47c37e9703d2eb79fd14a
serb2331 <serban.foica@gmail.com>
2025-05-18 18:19:44 +0300

    Add page implementation and some cleaning up  - add page implementation at route /Add/Drink  - new AddController + AddDrinkViewModel

--- WinUIApp.WebUI/Controllers/HomeController.cs
+++ WinUIApp.WebUI/Controllers/HomeController.cs
@@ -1,7 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using WinUIApp.ProxyServices;
-using WinUIApp.WebMVC.Models;
+using WinUIApp.WebUI.Models;
 
 namespace WinUIApp.WebMVC.Controllers;
 
